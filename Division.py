ff10e0460a22f03753365d3f16c96e31a086f653
Ham乙a Wa丂eem <120158214+Ninja-hamza@users.noreply.github.com>
2023-08-07 20:06:55 +0500

    Add files via upload

---
+++ Division.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    integerDivision = int(a / b)
+    floatDivision = float(a/b)
+    print(integerDivision)
+    print(floatDivision)
+    
